c9921ce85bb0ed419b9efbbc53b79259dfd548d8
Aadil Sengupta <53567039+aadil-sengupta@users.noreply.github.com>
2025-06-23 20:16:05 +0530

    Fix apps not loaded error.

--- django/seika/seika/asgi.py
+++ django/seika/seika/asgi.py
@@ -13,10 +13,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-from seika.routing import websocket_urlpatterns
 from seika.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seika.settings')
+
+from seika.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
